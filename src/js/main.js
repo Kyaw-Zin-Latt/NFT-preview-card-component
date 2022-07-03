6f3431f498fe8a33d80be979077acb84e36540b9
Kyaw Zin Latt <kyawzinlat43021@gmail.com>
2022-07-03 16:51:47 +0630

    finish

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+
+// Import our custom CSS
+import '../scss/styles.scss'
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
